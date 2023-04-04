b2172926ef675bbd28768045c7cccd8216207315
Patrick Martins <xavier.patrickmartins@gmail.com>
2023-04-04 09:58:54 -0300

    Commit dos exercícios para prática propostos na aula de Ambiente e Hello World

--- core/views.py
+++ core/views.py
@@ -4,3 +4,23 @@ from django.shortcuts import render, HttpResponse
 
 def hello(request, nome, idade):
     return HttpResponse(f'<h1>Hello {nome} com {idade} anos</h1>')
+
+
+def soma(request, num1, num2):
+    sum = num1 + num2
+    return HttpResponse(f"<h1>{num1} + {num2} = {soma}</h1>")
+
+
+def multiplicacao(request, num1, num2):
+    multiply = num1 * num2
+    return HttpResponse(f"<h1>{num1} + {num2} = {multiply}</h1>")
+
+
+def divisao(request, num1, num2):
+    division = num1 / num2
+    return HttpResponse(f"<h1>{num1} + {num2} = {division}</h1>")
+
+
+def subtracao(request, num1, num2):
+    subtract = num1 - num2
+    return HttpResponse(f"<h1>{num1} + {num2} = {subtract}</h1>")
